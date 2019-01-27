5d83b7627d96fd12efcdbc1857d3f29344590981
tejaAPI <brahmateja.bollineni@gmail.com>
2019-01-27 14:41:59 +0530

    third commit

--- client.py
+++ client.py
@@ -26,12 +26,39 @@ def init(address):
 	
 
 def list_connections(cmd):
+	global Friends_list
 	sock.send(str.encode(cmd))
 	Friends_list=sock.recv(1024)
 	Friends_list=eval(Friends_list.decode("utf-8"))
 	for i,x in enumerate(Friends_list):
 		print(i," ",x)
+def chat_with_ur_friend(addr):
+	while True:
+		
+		sock.send(bytes(input(""),'utf-8'))
+		data=sock.recv(1024)
+		print(data)
 	
+	
+
+
+
+
+
+
+def req_to_target_client(addr):
+	try:
+		print("entered")
+		print(addr)
+		sock.send(str.encode(str(addr)))
+		addr=sock.recv(1024)
+		addr=eval(addr.decode("utf-8"))
+		print(type(addr[0]))
+		print(type(addr[1]))
+
+		chat_with_ur_friend(addr)
+	except:
+		print("exception in targetclient")
 	
 
 def start_chat():
@@ -42,9 +69,10 @@ def start_chat():
 		if cmd == 'list':
 			list_connections(cmd)
 		elif 'select' in cmd:
-			conn = get_target(cmd)
-			if conn is not None:
-				send_to_target_client(conn)
+			cmd=cmd.split()
+			addr = Friends_list[int(cmd[1])]
+			if addr is not None:
+				req_to_target_client(addr)
 	
 	
 	
